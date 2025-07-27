1b08bef01230a074b60e8aff4dd029a8abbb91c8
Rasin <rasinc258@gmail.com>
2025-07-27 19:57:13 +0530

    added merge.py

--- merge.py
+++ merge.py
@@ -4,11 +4,12 @@ import pandas as pd
 df_component = pd.read_csv('dataset/merge/df_combined_with_components.csv')
 df_bsh = pd.read_csv('dataset/merge/BSH_compound_protein_bile_acid_detailed 12.csv')
 
-protein_inhibitor_cols = [col for col in df_bsh.columns if col.startswith('Protein_') or col.startswith('Inhibitor_')]
+protein_cols = [col for col in df_bsh.columns if col.startswith('Protein_')]
+inhibitor_cols = [col for col in df_bsh.columns if col.startswith('Inhibitor_')]
 
-df_protein_inhibitor= df_bsh[['BSH_Enzyme'] + protein_inhibitor_cols]
-
-df_protein_inhibitor_merged = pd.merge(df_component, df_protein_inhibitor, on='BSH_Enzyme', how='left').drop_duplicates()
+df_protein = df_bsh[['BSH_Enzyme'] + protein_cols].drop_duplicates()
+df_inhibitor = df_bsh[['Inhibitor'] + inhibitor_cols].drop_duplicates()
+df_inhibitor = df_inhibitor[df_inhibitor['Inhibitor'] != 'VI']
 
 properties = [
     'complexity',
@@ -19,17 +20,24 @@ properties = [
     'polar_surface_area'
 ]
 
-prop_cols = [c for c in df_bsh.columns if any(c.endswith(f"_{prop}") for prop in properties)]
-df_bile_acid_props = df_bsh[prop_cols].drop_duplicates()
+Bile_Acids = ['GCA', 'GCDCA', 'GDCA', 'GLCA', 'GUDCA', 'TCA', 'TCDCA', 'TDCA',
+              'TLCA', 'TUDCA']
 
-df_protein_inhibitor_bile_acid = df_protein_inhibitor_merged.copy()
+Bile_Acid_rows = []
 
-for prop in properties:
-    def get_prop_value(row):
-        bile_acid = row['Bile_Acid']
-        col_name = f"{bile_acid}_{prop}"
-        return df_bile_acid_props.at[0, col_name]
+for acid in Bile_Acids:
+    row = {'Bile_Acid': acid}
+    for prop in properties:
+        col_name = f"{acid}_{prop}"
+        row[prop] = df_bsh[col_name].iloc[0]  # Assuming single-row source
+    Bile_Acid_rows.append(row)
 
-    df_protein_inhibitor_bile_acid[prop] = df_protein_inhibitor_merged.apply(get_prop_value, axis=1)
+df_bile_acid = pd.DataFrame(Bile_Acid_rows)
 
-print('break')
+
+df_protein_inhibitor_bile_acid = df_component.copy()
+df_protein_inhibitor_bile_acid = df_protein_inhibitor_bile_acid.merge(df_protein, on='BSH_Enzyme', how='left')
+df_protein_inhibitor_bile_acid = df_protein_inhibitor_bile_acid.merge(df_inhibitor, on='Inhibitor', how='left')
+df_protein_inhibitor_bile_acid = df_protein_inhibitor_bile_acid.merge(df_bile_acid, on='Bile_Acid', how='left')
+
+df_protein_inhibitor_bile_acid.to_csv('dataset/generated/df_protein_inhibitor_bile_acid.csv')
